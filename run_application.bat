7e41f4d4e0fa6214aee8c236279b649ba341c3cb
Marcelo Kafels <marcelokafels@gmail.com>
2020-08-10 21:14:35 -0300

    Update run_application.bat

--- run_application.bat
+++ run_application.bat
@@ -1,3 +1,7 @@
-python -m pip install pywin32 win32/pyHook-1.5.1-cp36-cp36m-win32.whl
+setlocal
+cd /d %~dp0
+
+python -m pip install pywin32 
+python -m pip install win32\pyHook-1.5.1-cp36-cp36m-win32.whl
 python -m pip install PyUserInput
 python main.py
